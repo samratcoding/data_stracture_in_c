cc04503b137e386a8c5b0ff66700670e08925704
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-03-22 05:24:21 +0600

    Create 05. singly_link_list.cpp

---
+++ Semester 03/05. singly_link_list.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    int item, n;
+    Node *fptr = nullptr, *eptr = nullptr, *nptr = nullptr;
+
+    cout << "Enter number of Nodes:" << endl;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << "Enter value for node " << i << ":" << endl;
+        cin >> item;
+
+        nptr = new Node;
+        nptr->data = item;
+        nptr->next = nullptr;
+
+        if (fptr == nullptr) {
+            fptr = nptr;
+            eptr = nptr;
+        } else {
+            eptr->next = nptr;
+            eptr = nptr;
+        }
+    }
+
+    // Display data
+    cout << "Linked List values:" << endl;
+    Node* current = fptr;
+    while (current != nullptr) {
+        cout << current->data << " -> ";
+        current = current->next;
+    }
+    cout << "NULL" << endl;
+
+    // Free memory (not required in this simple example, but good practice)
+    current = fptr;
+    while (current != nullptr) {
+        Node* temp = current;
+        current = current->next;
+        delete temp;
+    }
+
+    return 0;
+}
