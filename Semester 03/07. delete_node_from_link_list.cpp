52856e385719e45315a3189d26c2c8dfd09f0603
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-03-22 10:35:09 +0600

    Update 07. delete_node_from_link_list.cpp

--- Semester 03/07. delete_node_from_link_list.cpp
+++ Semester 03/07. delete_node_from_link_list.cpp
@@ -1,3 +1,39 @@
+// Singly Linklist
+void deleteNode(Node*& fptr, int value) {
+    if (fptr == nullptr) {
+        cout << "List is empty. Cannot delete." << endl;
+        return;
+    }
+
+    Node* current = fptr;
+    Node* prev = nullptr;
+
+    // Find the node to delete and keep track of its previous node
+    while (current != nullptr && current->data != value) {
+        prev = current;
+        current = current->next;
+    }
+
+    // If the node with the specified value is not found
+    if (current == nullptr) {
+        cout << "Node with value " << value << " not found. Cannot delete." << endl;
+        return;
+    }
+
+    // If the node to delete is the first node
+    if (prev == nullptr) {
+        fptr = current->next;
+    } else {
+        prev->next = current->next;
+    }
+
+    delete current;
+    cout << "Node with value " << value << " deleted successfully." << endl;
+}
+
+
+// Double
+
 void deleteNode(Node*& fptr, Node*& eptr, int value) {
     if (fptr == nullptr) {
         cout << "List is empty. Cannot delete." << endl;
