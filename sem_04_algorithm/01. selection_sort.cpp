2f9ae5217566cc092d9992ac1f558601ea44907c
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-08-27 12:59:51 +0600

    Create 01. selection_sort.cpp

---
+++ sem_04_algorithm/01. selection_sort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n = 7, temp, i, j;
+  int data[n] = {7,3,2,9,5,10,12};
+
+
+  for(i = 0; i<n; i++){
+
+    int small_i = i;
+
+    for(j=i+1; j<n; j++){
+
+     if(data[j] < data[small_i]){
+        small_i = j;
+     } // end condition
+
+    } // end inner loop
+    j = i+i;
+    temp = data[i];
+    data[i] = data[small_i];
+    data[small_i] = temp;
+
+  } //end outer loop
+
+
+
+  for(int i = 0; i<n; i++){
+    cout << data[i] << "\n";
+  }
+
+
+} // end main function
